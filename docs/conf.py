aaf96b089bfad9c90a860f1c3e2b9e6e1b96a3a2
Kevin <kevin.krechan@hochfrequenz.de>
2021-08-30 18:45:28 +0200

    Add config for extension apidoc

--- docs/conf.py
+++ docs/conf.py
@@ -44,3 +44,10 @@ html_theme = "alabaster"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+
+# -- sphinxcontrib-apidoc settings -------------------------------------------
+apidoc_module_dir = "../src/ahbicht"
+apidoc_output_dir = "api"
+apidoc_excluded_paths = ["unittests"]
+apidoc_separate_modules = True
