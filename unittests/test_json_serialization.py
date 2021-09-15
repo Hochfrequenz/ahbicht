ac9838df2b1e076c325f4d129b848914a0eaea27
konstantin <konstantin.klein@hochfrequenz.de>
2021-09-15 18:25:35 +0200

    improve json serialization test case

--- unittests/test_json_serialization.py
+++ unittests/test_json_serialization.py
@@ -11,7 +11,7 @@ from ahbicht.json_serialization.tree_schema import TreeSchema
 
 class TestJsonSerialization:
     @pytest.mark.parametrize(
-        "tree",
+        "tree, expected_json_dict",
         [
             pytest.param(
                 Tree(
@@ -23,15 +23,40 @@ class TestJsonSerialization:
                             [Tree("condition_key", [Token("INT", "1")]), Tree("condition_key", [Token("INT", "2")])],
                         ),
                     ],
-                )
+                ),
+                {
+                    "type": "or_composition",
+                    "children": [
+                        {
+                            "string": None,
+                            "tree": {"children": [{"string": "53", "tree": None}], "type": "condition_key"},
+                        },
+                        {
+                            "string": None,
+                            "tree": {
+                                "children": [
+                                    {
+                                        "string": None,
+                                        "tree": {"children": [{"string": "1", "tree": None}], "type": "condition_key"},
+                                    },
+                                    {
+                                        "string": None,
+                                        "tree": {"children": [{"string": "2", "tree": None}], "type": "condition_key"},
+                                    },
+                                ],
+                                "type": "and_composition",
+                            },
+                        },
+                    ],
+                },
             )
         ],
     )
-    def test_tree_serialization(self, tree: Tree):
-        tree_dict: dict = TreeSchema().dump(tree)
-        assert len(tree_dict.keys()) > 0
-        json_string = json.dumps(tree_dict)
+    def test_tree_serialization(self, tree: Tree, expected_json_dict: dict):
+        json_string = TreeSchema().dumps(tree)
         assert json_string is not None
+        actual_json_dict = json.loads(json_string)
+        assert actual_json_dict == expected_json_dict
         deserialized_tree = TreeSchema().loads(json_data=json_string)
         assert isinstance(deserialized_tree, Tree)
         assert deserialized_tree == tree
