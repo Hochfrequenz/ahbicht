cf6a99bb32d986315075fafc900dc3f8fefb8805
konstantin <konstantin.klein@hochfrequenz.de>
2021-09-23 08:05:14 +0200

    add guid

--- unittests/test_json_serialization.py
+++ unittests/test_json_serialization.py
@@ -2,12 +2,18 @@
 Tests that the parsed trees are JSON serializable
 """
 import json
+import uuid
 
 import pytest
 from lark import Token, Tree
 
+from ahbicht.content_evaluation.content_evaluation_result import ContentEvaluationResult, ContentEvaluationResultSchema
 from ahbicht.expressions.ahb_expression_parser import parse_ahb_expression_to_single_requirement_indicator_expressions
-from ahbicht.expressions.condition_nodes import EvaluatedFormatConstraint, EvaluatedFormatConstraintSchema
+from ahbicht.expressions.condition_nodes import (
+    ConditionFulfilledValue,
+    EvaluatedFormatConstraint,
+    EvaluatedFormatConstraintSchema,
+)
 from ahbicht.json_serialization.tree_schema import TreeSchema
 
 
@@ -126,7 +132,7 @@ class TestJsonSerialization:
             ),
         ],
     )
-    def test_evaluated_format_constraint_evaluation(
+    def test_evaluated_format_constraint_serialization(
         self, evaluated_format_constraint: EvaluatedFormatConstraint, expected_json_dict: dict
     ):
         json_string = EvaluatedFormatConstraintSchema().dumps(evaluated_format_constraint)
@@ -136,3 +142,46 @@ class TestJsonSerialization:
         deserialized_constraint = EvaluatedFormatConstraintSchema().loads(json_data=json_string)
         assert isinstance(deserialized_constraint, EvaluatedFormatConstraint)
         assert deserialized_constraint == evaluated_format_constraint
+
+    @pytest.mark.parametrize(
+        "content_evaluation_result, expected_json_dict",
+        [
+            pytest.param(
+                ContentEvaluationResult(
+                    hints={"501": "foo", "502": "bar", "503": None},
+                    format_constraints={
+                        "901": EvaluatedFormatConstraint(
+                            format_constraint_fulfilled=False, error_message="something is wrong"
+                        ),
+                        "902": EvaluatedFormatConstraint(format_constraint_fulfilled=True, error_message=None),
+                    },
+                    requirement_constraints={
+                        "1": ConditionFulfilledValue.NEUTRAL,
+                        "2": ConditionFulfilledValue.FULFILLED,
+                        "3": ConditionFulfilledValue.UNFULFILLED,
+                        "4": ConditionFulfilledValue.UNKNOWN,
+                    },
+                    id=uuid.UUID("d106f335-f663-4d14-9636-4f43a883ad26"),
+                ),
+                {
+                    "hints": {"501": "foo", "502": "bar", "503": None},
+                    "format_constraints": {
+                        "902": {"format_constraint_fulfilled": True, "error_message": None},
+                        "901": {"format_constraint_fulfilled": False, "error_message": "something is wrong"},
+                    },
+                    "requirement_constraints": {"1": "NEUTRAL", "2": "FULFILLED", "3": "UNFULFILLED", "4": "UNKNOWN"},
+                    "id": "d106f335-f663-4d14-9636-4f43a883ad26",
+                },
+            ),
+        ],
+    )
+    def test_content_evaluation_result_serialization(
+        self, content_evaluation_result: ContentEvaluationResult, expected_json_dict: dict
+    ):
+        json_string = ContentEvaluationResultSchema().dumps(content_evaluation_result)
+        assert json_string is not None
+        actual_json_dict = json.loads(json_string)
+        assert actual_json_dict == expected_json_dict
+        deserialized_result = ContentEvaluationResultSchema().loads(json_data=json_string)
+        assert isinstance(deserialized_result, ContentEvaluationResult)
+        assert deserialized_result == content_evaluation_result
