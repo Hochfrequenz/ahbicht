e0e02f7191b1d81dde068ad849598acc5761ef56
konstantin <konstantin.klein@hochfrequenz.de>
2021-10-11 18:17:11 +0200

    assert the right method is called

--- unittests/test_dict_based_fc_evaluator.py
+++ unittests/test_dict_based_fc_evaluator.py
@@ -1,4 +1,5 @@
 """ Tests the dictionary based FC evaluator"""
+from unittest import mock
 
 import pytest
 
@@ -11,7 +12,7 @@ pytestmark = pytest.mark.asyncio
 class TestDictBasedFcEvaluator:
     """Test for the evaluation using the Dict Based FC Evaluator"""
 
-    async def test_evaluation(self):
+    async def test_evaluation(self, mocker):
         hardcoded_results = {
             "1": EvaluatedFormatConstraint(format_constraint_fulfilled=True),
             "2": EvaluatedFormatConstraint(format_constraint_fulfilled=False, error_message="something wrong"),
@@ -25,5 +26,6 @@ class TestDictBasedFcEvaluator:
         )
         with pytest.raises(NotImplementedError):
             await evaluator.evaluate_single_format_constraint("3", entered_input="qwe")
-
+        dict_evaluation_mock = mocker.spy(evaluator, "evaluate_single_format_constraint")
         assert await evaluator.evaluate_format_constraints(["1", "2"], entered_input="asd") == hardcoded_results
+        dict_evaluation_mock.assert_has_awaits([mock.call("1", "asd"), mock.call("2", "asd")])
