e247b691af7510d5f66858ef8e42c4e5f9d3e97a
konstantin <konstantin.klein@hochfrequenz.de>
2021-10-19 13:40:13 +0200

    🐛 Fix UnboundLocalError (#51)

--- unittests/test_evaluator_factory.py
+++ unittests/test_evaluator_factory.py
@@ -1,7 +1,9 @@
 """ Tests the Evaluator Factory"""
 import uuid
+from typing import Optional
 
 import pytest
+from _pytest.fixtures import SubRequest
 
 from ahbicht.content_evaluation.content_evaluation_result import ContentEvaluationResult
 from ahbicht.content_evaluation.evaluator_factory import create_and_inject_hardcoded_evaluators
@@ -15,28 +17,57 @@ pytestmark = pytest.mark.asyncio
 class TestEvaluatorFactory:
     """Tests, that evaluators are created and injected correctly"""
 
-    @pytest.fixture()
-    def setup_and_teardown_injector(self):
-        content_eval_result = ContentEvaluationResult(
-            hints={"501": "foo"},
-            format_constraints={
-                "902": EvaluatedFormatConstraint(format_constraint_fulfilled=True, error_message=None),
-            },
-            requirement_constraints={
-                "2": ConditionFulfilledValue.FULFILLED,
-                "3": ConditionFulfilledValue.UNFULFILLED,
-            },
-            id=uuid.UUID("d106f335-f663-4d14-9636-4f43a883ad26"),
-        )
-        create_and_inject_hardcoded_evaluators(content_eval_result)
+    @pytest.fixture
+    def inject_content_evaluation_result(self, request: SubRequest):
+        # indirect parametrization: https://stackoverflow.com/a/33879151/10009545
+        content_evaluation_result = request.param
+        create_and_inject_hardcoded_evaluators(content_evaluation_result=content_evaluation_result)
 
-    def test_correct_injection(self, setup_and_teardown_injector):
-        tree = parse_ahb_expression_to_single_requirement_indicator_expressions("Muss ([2] O [3])[902]U[501]")
+    @pytest.mark.parametrize(
+        "inject_content_evaluation_result",
+        [
+            ContentEvaluationResult(
+                hints={"501": "foo"},
+                format_constraints={
+                    "902": EvaluatedFormatConstraint(format_constraint_fulfilled=True, error_message=None),
+                },
+                requirement_constraints={
+                    "2": ConditionFulfilledValue.FULFILLED,
+                    "3": ConditionFulfilledValue.UNFULFILLED,
+                },
+                id=uuid.UUID("d106f335-f663-4d14-9636-4f43a883ad26"),
+            )
+        ],
+        indirect=True,
+    )
+    @pytest.mark.parametrize(
+        "expression, expected_requirement_indicator, expected_format_constraint_result, expected_in_hints",
+        [
+            pytest.param("Muss ([2] O [3])[902]U[501]", "Muss", True, "foo"),
+            pytest.param("Muss [2] O [3][902]U[501]", "Muss", True, None),
+        ],
+    )
+    def test_correct_injection(
+        self,
+        inject_content_evaluation_result,
+        expression: str,
+        expected_requirement_indicator: str,
+        expected_format_constraint_result: bool,
+        expected_in_hints: Optional[str],
+    ):
+        tree = parse_ahb_expression_to_single_requirement_indicator_expressions(ahb_expression=expression)
+        assert tree is not None
         expression_evaluation_result = evaluate_ahb_expression_tree(tree, entered_input="hello")
         assert (
             expression_evaluation_result.requirement_constraint_evaluation_result.requirement_constraints_fulfilled
             is True
         )
-        assert expression_evaluation_result.requirement_indicator == "Muss"
-        assert expression_evaluation_result.format_constraint_evaluation_result.format_constraints_fulfilled is True
-        assert "foo" in expression_evaluation_result.requirement_constraint_evaluation_result.hints
+        assert expression_evaluation_result.requirement_indicator == expected_requirement_indicator
+        assert (
+            expression_evaluation_result.format_constraint_evaluation_result.format_constraints_fulfilled
+            is expected_format_constraint_result
+        )
+        if expected_in_hints:
+            assert expected_in_hints in expression_evaluation_result.requirement_constraint_evaluation_result.hints
+        else:
+            assert expression_evaluation_result.requirement_constraint_evaluation_result.hints is None
