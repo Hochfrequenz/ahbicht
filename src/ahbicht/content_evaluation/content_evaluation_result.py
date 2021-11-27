2891432d8646a0b07c5f056ae4ddd5873c5ba633
konstantin <konstantin.klein@hochfrequenz.de>
2021-11-27 23:32:56 +0100

    wip the feature

--- src/ahbicht/content_evaluation/content_evaluation_result.py
+++ src/ahbicht/content_evaluation/content_evaluation_result.py
@@ -1,7 +1,7 @@
 """
 This module contains a class to store _all_ kinds of content evaluation results.
 """
-from itertools import product
+from itertools import combinations, product
 from typing import Dict, List, Optional
 from uuid import UUID
 
@@ -48,7 +48,7 @@ class ContentEvaluationResultSchema(Schema):
     requirement_constraints = fields.Dict(
         keys=fields.String(allow_none=False), values=fields.String(allow_none=True), required=True
     )
-    id = fields.UUID(required=False, dump_default=False)
+    id = fields.UUID(required=False, dump_default=False, missing=None)
 
     @post_load
     def deserialize(self, data, **kwargs) -> ContentEvaluationResult:
@@ -113,23 +113,44 @@ class ContentEvaluationPrerequisites:
         messages, resolving packages.
         """
         results: List[ContentEvaluationResult] = []
-        for rcfc_tuple in product(
-            [x for x in product(["dummy"] + self.requirement_constraint_keys, ConditionFulfilledValue)],
-            [x for x in product(["dummy"] + self.format_constraint_keys, [True, False])],
-        ):
-            # we need the dummys to run into this loop even if one of either fc_keys or rc_keys is empty
+        if len(self.format_constraint_keys) == 0 and len(self.requirement_constraint_keys) == 0:
+            return results
+        # for easier debugging below, replace the generators "(" with a materialized lists "["
+        if len(self.format_constraint_keys) > 0:
+            possible_fcs = (
+                z
+                for z in combinations(
+                    product(self.format_constraint_keys, [True, False]), len(self.format_constraint_keys)
+                )
+                if len({y[0] for y in z}) == len(self.format_constraint_keys)
+            )
+        else:
+            possible_fcs = [(("fc_dummy", True),)]  # type:ignore[assignment]
+
+        if len(self.requirement_constraint_keys) > 0:
+            possible_rcs = (
+                z
+                for z in combinations(
+                    product(self.requirement_constraint_keys, ConditionFulfilledValue),
+                    len(self.requirement_constraint_keys),
+                )
+                if len({y[0] for y in z}) == len(self.requirement_constraint_keys)
+            )
+        else:
+            possible_rcs = [(("rc_dummy", ConditionFulfilledValue.NEUTRAL),)]  # type:ignore[assignment]
+        for fc_rc_tuple in product(possible_fcs, possible_rcs):
+            # This product would have length 0 if one of the "factors" had length 0.
+            # In order to prevent 'results' to be empty if either the RC or FC list is empty, we added the the 'dummy's.
             result = ContentEvaluationResult(
                 hints={hint_key: f"Hinweis {hint_key}" for hint_key in self.hint_keys},
-                requirement_constraints={
-                    rc_tuple[0][0]: rc_tuple[1] for rc_tuple in rcfc_tuple[0] if rc_tuple[0] != "dummy"
-                },
                 format_constraints={
-                    fc_tuple[0]: EvaluatedFormatConstraint(format_constraint_fulfilled=fc_tuple[1])
-                    for fc_tuple in rcfc_tuple[1]
-                    if fc_tuple[0] != "dummy"
+                    fc_kvp[0]: EvaluatedFormatConstraint(format_constraint_fulfilled=fc_kvp[1])
+                    for fc_kvp in fc_rc_tuple[0]
+                    if fc_kvp[0] != "fc_dummy"
                 },
+                requirement_constraints={rc_kvp[0]: rc_kvp[1] for rc_kvp in fc_rc_tuple[1] if rc_kvp[0] != "rc_dummy"},
             )
-            result.append(result)
+            results.append(result)
         return results
 
 
--- unittests/test_prerequisites_generation.py
+++ unittests/test_prerequisites_generation.py
@@ -1,11 +1,17 @@
 """ Tests for the parsing of the conditions tests (Mussfeldprüfung) """
+import json
 from typing import List
 
 import pytest  # type:ignore[import]
 
-from ahbicht.content_evaluation.content_evaluation_result import ContentEvaluationPrerequisites
+from ahbicht.content_evaluation.content_evaluation_result import (
+    ContentEvaluationPrerequisites,
+    ContentEvaluationPrerequisitesSchema,
+    ContentEvaluationResult,
+    ContentEvaluationResultSchema,
+)
 from ahbicht.expressions.condition_expression_parser import collect_prerequisites
-from ahbicht.expressions.condition_nodes import ConditionFulfilledValue
+from ahbicht.expressions.condition_nodes import ConditionFulfilledValue, EvaluatedFormatConstraint
 
 
 class TestPreqrequisitesGeneration:
@@ -52,6 +58,13 @@ class TestPreqrequisitesGeneration:
         [
             pytest.param(
                 ContentEvaluationPrerequisites(
+                    hint_keys=[], requirement_constraint_keys=[], format_constraint_keys=[], package_keys=[]
+                ),
+                [],
+                id="0 FC, 0 RC",
+            ),
+            pytest.param(
+                ContentEvaluationPrerequisites(
                     hint_keys=[], requirement_constraint_keys=["1"], format_constraint_keys=[], package_keys=[]
                 ),
                 [
@@ -72,11 +85,53 @@ class TestPreqrequisitesGeneration:
                         hints={}, format_constraints={}, requirement_constraints={"1": ConditionFulfilledValue.NEUTRAL}
                     ),
                 ],
-            )
+                id="0 FC, 1 RC",
+            ),
+            pytest.param(
+                ContentEvaluationPrerequisites(
+                    hint_keys=[], requirement_constraint_keys=[], format_constraint_keys=["901"], package_keys=[]
+                ),
+                [
+                    ContentEvaluationResult(
+                        hints={},
+                        format_constraints={"901": EvaluatedFormatConstraint(format_constraint_fulfilled=True)},
+                        requirement_constraints={},
+                    ),
+                    ContentEvaluationResult(
+                        hints={},
+                        format_constraints={"901": EvaluatedFormatConstraint(format_constraint_fulfilled=False)},
+                        requirement_constraints={},
+                    ),
+                ],
+                id="1 FC, 0 RC",
+            ),
         ],
     )
-    def test_possible_cer_generation(
+    def test_possible_cer_generation_small_results(
         self, prerequisites: ContentEvaluationPrerequisites, expected_cers: List[ContentEvaluationResult]
     ):
         actual = prerequisites.generate_possible_content_evaluation_results()
+        # We only test the small edge cases as real code.
+        # This quickly gets super large. 2 FCs * 2 RCs is already 64 results
         assert actual == expected_cers
+
+    ALL_LARGE_TEST_CASES = pytest.mark.datafiles(
+        "./unittests/content_evaluation_result_generation/example0.json",
+        "./unittests/content_evaluation_result_generation/example1.json",
+    )
+
+    @pytest.mark.parametrize(
+        "test_file_path",
+        [
+            pytest.param("example0.json", id="0 FC, 3 RC"),
+            pytest.param("example1.json", id="2 FC, 3 RC"),
+        ],
+    )
+    @ALL_LARGE_TEST_CASES
+    def test_possible_cer_generation_large_results(self, test_file_path, datafiles):
+        file_content = json.load(datafiles / test_file_path)
+        prerequisites = ContentEvaluationPrerequisitesSchema().load(file_content["prerequisite"])
+        expected_result = ContentEvaluationResultSchema(many=True).load(file_content["expected_result"])
+        actual = prerequisites.generate_possible_content_evaluation_results()
+        json_string = ContentEvaluationResultSchema(many=True).dumps(actual)
+        assert actual == expected_result
