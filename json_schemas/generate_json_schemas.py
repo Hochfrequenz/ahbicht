72ae02b6679a6c08ea3902fcdec448a7f845c93f
konstantin <konstantin.klein@hochfrequenz.de>
2021-11-27 16:59:52 +0100

    update everything

--- json_schemas/generate_json_schemas.py
+++ json_schemas/generate_json_schemas.py
@@ -4,6 +4,7 @@ It creates json schema files as described in the README.md in the same directory
 """
 
 import json
+import pathlib
 from typing import List, Type
 
 from marshmallow import Schema
@@ -34,8 +35,10 @@ schema_types: List[Type[Schema]] = [
 ]
 json_schema = JSONSchema()
 for schema_type in schema_types:
+    this_directory = pathlib.Path(__file__).parent.absolute()
     file_name = schema_type.__name__ + ".json"  # pylint:disable=invalid-name
+    file_path = this_directory / file_name
     schema_instance = schema_type()
     json_schema_dict = json_schema.dump(schema_instance)
-    with open(file_name, "w", encoding="utf-8") as json_schema_file:
-        json.dump(json_schema_dict, json_schema_file, ensure_ascii=False, sort_keys=True, indent=4)
+    with open(file_path, "w", encoding="utf-8") as json_schema_file:
+        json.dump(json_schema_dict, json_schema_file, ensure_ascii=False, sort_keys=False, indent=4)
