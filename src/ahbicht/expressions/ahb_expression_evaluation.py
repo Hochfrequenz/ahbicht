21cd33d851bd504ac2f117bcb259d2b101306cc5
konstantin <konstantin.klein@hochfrequenz.de>
2022-01-03 14:50:28 +0100

    introduce prefix operator enum

--- src/ahbicht/expressions/ahb_expression_evaluation.py
+++ src/ahbicht/expressions/ahb_expression_evaluation.py
@@ -58,6 +58,45 @@ _str_to_modal_mark_mapping: Dict[str, ModalMark] = {
 }
 
 
+@unique
+class PrefixOperator(str, Enum):
+    """
+    Operator which does not function to combine conditions, but as requirement indicator.
+    It stands alone or in front of a condition expression. Please find detailed descriptions of the operators and their
+    usage in the "Allgemeine Festlegungen".
+    Note that with MaKo2022 introced 2022-04-01 the "O" and "U" prefix operators will be deprecated.
+    Refer to the "Allgemeine Festlegungen" valid up to 2022-04-01 for deprecated "O" and "U".
+    """
+
+    X = "X"
+    """
+    The "X" operator. See "Allgemeine Festlegungen" Kapitel 6.8.1. Usually this just means something is required
+    or required under circumstances defined in a trailing condition expression.
+    It shall be read as "exclusive or" regarding how qualifiers/codes shall be used from a finite set.
+    Note that "X" can also be used as "logical exclusive or" (aka "xor") operator in condition expressions.
+    The prefix operator works differently from the logical operator in condition expressions!
+    The usage of "X" as logical operator is deprecated since 2022-04-01. It will be replaced with the "⊻" symbol.
+    """
+    O = "O"
+    """
+    The "O" operator means that at least one out of multiple possible qualifiers/codes has to be given.
+    This is typically found when describing ways to contact a market partner (CTA): You can use email or phone or fax
+    but you have to provide at least one of the given possibilities.
+    The usage of "O" as a prefix operator is deprecated since 2022-04-01.
+    Note that "O" can also be used as a "logical or" (aka "lor") operator in condition expressions.
+    The prefix operator works differently from the logical operator in condition expressions!
+    The usage of "O" as logical operator is also deprecated since 2022-04-01. It will be replaced with the "∨" symbol.
+    """
+    U = "U"
+    """
+    The "U" operator means that all provided qualifiers/codes have to be used.
+    The usage of "U" as a prefix operator is deprecated since 2022-04-01.
+    Note that "U" can also be used as a "logical and" (aka "land") operator in condition expressions.
+    The prefix operator works differently from the logical operator in condition expressions!
+    The usage of "U" as logical operator is also deprecated since 2022-04-01. It will be replaced with the "∧" symbol.
+    """
+
+
 # pylint: disable=no-self-use, invalid-name
 class AhbExpressionTransformer(Transformer):
     """
@@ -80,9 +119,9 @@ class AhbExpressionTransformer(Transformer):
         """Returns the condition expression."""
         return condition_expression.value
 
-    def PREFIX_OPERATOR(self, prefix_operator: Token) -> str:
+    def PREFIX_OPERATOR(self, prefix_operator: Token) -> PrefixOperator:
         """Returns the prefix operator."""
-        return prefix_operator.value  # X, O, U,
+        return PrefixOperator(prefix_operator.value)
 
     def MODAL_MARK(self, modal_mark: Token) -> ModalMark:
         """Returns the modal mark."""
