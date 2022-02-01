cf9988406f1064b60903ee569d15cc4f22521186
Annika Schlögl <annika.schloegl@hochfrequenz.de>
2022-02-01 10:52:16 +0100

    ✨Add validation results

---
+++ src/ahbicht/validation/validation_results.py
@@ -0,0 +1,102 @@
+"This module contains the classes for the validation results."
+
+from abc import ABC
+from typing import List, Optional
+
+import attr
+from marshmallow import Schema, fields, post_load
+from marshmallow_enum import EnumField  # type:ignore[import]
+
+from ahbicht.validation.validation_values import FormatValidationValue, RequirementValidationValue
+
+# pylint: disable=too-few-public-methods, no-member, no-self-use, unused-argument
+
+
+@attr.s(auto_attribs=True, kw_only=True)
+class ValidationResult(ABC):
+    """Result of the validation"""
+
+    #: In which requirement state is the field and is it filled or not?
+    requirement_validation: RequirementValidationValue
+    #: Collected hints
+    hints: Optional[str] = attr.ib(default=None)
+
+
+@attr.s(auto_attribs=True, kw_only=True)
+class SegmentLevelValidationResult(ValidationResult):
+    """Result of the validation of a segment or segment group"""
+
+
+@attr.s(auto_attribs=True, kw_only=True)
+class DataElementValidationResult(ValidationResult):
+    """Result of the validation of a dataelement"""
+
+    #: Is the format constraint fulfilled or not?
+    format_validation: FormatValidationValue = attr.ib(validator=attr.validators.instance_of(FormatValidationValue))
+    #: possible error message regarding the format
+    format_error_message: Optional[str] = attr.ib(default=None)
+    #: possible qualifiers for value pool dataelements
+    possible_values: Optional[List[str]] = attr.ib(
+        default=None,
+        validator=attr.validators.optional(
+            attr.validators.deep_iterable(
+                member_validator=attr.validators.instance_of(str),
+                iterable_validator=attr.validators.instance_of(list),
+            )
+        ),
+    )
+
+
+class ValidationResultSchema(Schema):
+    """
+    A schema to (de-)serialize ValidationResult
+    """
+
+    requirement_validation = EnumField(RequirementValidationValue)
+    hints = fields.String(load_default=None)
+
+    @post_load
+    def deserialize(self, data, **kwargs) -> ValidationResult:
+        """
+        Converts the barely typed data dictionary into an actual ValidationResult
+        :param data:
+        :param kwargs:
+        :return:
+        """
+        return ValidationResult(**data)
+
+
+class SegmentLevelValidationResultSchema(ValidationResultSchema):
+    """
+    A schema to (de-)serialize SegmentLevelValidationResult
+    """
+
+    @post_load
+    def deserialize(self, data, **kwargs) -> SegmentLevelValidationResult:
+        """
+        Converts the barely typed data dictionary into an actual ValidationResult
+        :param data:
+        :param kwargs:
+        :return:
+        """
+        return SegmentLevelValidationResult(**data)
+
+
+class DataElementValidationResultSchema(ValidationResultSchema):
+    """
+    A schema to (de-)serialize DataElementValidationResult
+    """
+
+    format_validation = EnumField(FormatValidationValue)
+    format_error_message = fields.String(load_default=None)
+    possible_values = fields.List(fields.Str, load_default=None)
+
+    @post_load
+    def deserialize(self, data, **kwargs) -> DataElementValidationResult:
+        """
+        Converts the barely typed data dictionary into an actual DataElementValidationResult
+        :param data:
+        :param kwargs:
+        :return:
+        """
+        return DataElementValidationResult(**data)
