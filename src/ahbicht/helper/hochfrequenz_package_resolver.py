0dcf6974d5759325969aeddee4e0443b3ec7bac4
konstantin <konstantin.klein@hochfrequenz.de>
2022-02-18 12:26:05 +0100

    add a package resolver for the hochfrequenz api

---
+++ src/ahbicht/helper/hochfrequenz_package_resolver.py
@@ -0,0 +1,38 @@
+"""
+Contains a Package Resolver that uses the Hochfrequenz API to resolve package keys to condition expressions
+"""
+import aiohttp
+
+from ahbicht.expressions.package_expansion import PackageResolver
+from ahbicht.mapping_results import PackageKeyConditionExpressionMapping, PackageKeyConditionExpressionMappingSchema
+
+
+class HochfrequenzPackageResolver(PackageResolver):
+    """
+    A package resolver that uses a REST API (by Hochfrequenz) to resolve packages.
+    Note that this resolver requires an internet connection to work and the hochfrequenz API to be up and running.
+    Consider using this resolver to retrieve package information once and then dump them into something fast and stable
+    like f.e. a JSON file, a database or feed its results into a hardcoded package resolver once on startup.
+    Relying on external web services is prone to be a bottleneck for your application.
+    """
+
+    _hochfrequenz_base_uri = "https://ahbicht.azurewebsites.net/api/ResolvePackageConditionExpression/"
+
+    def __init__(self, api_url=_hochfrequenz_base_uri):
+        """
+        initializes the package resolver and set's up a cache
+        """
+        self.api_url = api_url
+
+    async def get_condition_expression(self, package_key: str) -> PackageKeyConditionExpressionMapping:
+        request_url = f"{self.api_url}/{self.edifact_format_version}/{self.edifact_format}/{package_key}"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(request_url) as response:
+                print("Status:", response.status)
+                if response.status != 200:
+                    return PackageKeyConditionExpressionMapping(
+                        package_key=package_key, package_expression=None, edifact_format=self.edifact_format
+                    )
+                response_body = await response.json()
+                result = PackageKeyConditionExpressionMappingSchema().load(response_body)
+                return result
---
+++ unittests/test_hochfrequenz_package_resolver.py
@@ -0,0 +1,47 @@
+"""
+Tests the HochfrequenzPackageResolver
+"""
+import pytest  # type:ignore[import]
+from aioresponses import CallbackResult, aioresponses
+
+# from maus.edifact import EdifactFormat, EdifactFormatVersion
+from ahbicht.edifact import EdifactFormat, EdifactFormatVersion
+from ahbicht.helper.hochfrequenz_package_resolver import HochfrequenzPackageResolver
+from ahbicht.mapping_results import PackageKeyConditionExpressionMapping
+
+pytestmark = pytest.mark.asyncio
+
+
+class TestHochfrequenzPackageResolver:
+    async def test_hochfrequenz_package_api_success(self):
+        package_resolver = HochfrequenzPackageResolver(api_url="https://test.inv")
+        package_resolver.edifact_format = EdifactFormat.UTILMD
+        package_resolver.edifact_format_version = EdifactFormatVersion.FV2204
+        with aioresponses() as mocked_server:
+            mocked_server.get(
+                "https://test.inv/FV2204/UTILMD/10P",
+                payload={"package_expression": "[20] \u2227 [244]", "package_key": "10P", "edifact_format": "UTILMD"},
+            )
+            actual = await package_resolver.get_condition_expression("10P")
+            assert actual == PackageKeyConditionExpressionMapping(
+                edifact_format=EdifactFormat.UTILMD, package_key="10P", package_expression="[20] ∧ [244]"
+            )
+
+    async def test_hochfrequenz_package_api_failure(self):
+        package_resolver = HochfrequenzPackageResolver(api_url="https://test.inv")
+        package_resolver.edifact_format = EdifactFormat.UTILMD
+        package_resolver.edifact_format_version = EdifactFormatVersion.FV2204
+
+        def simulate_error(url, **kwargs):
+            return CallbackResult(status=400, payload={"see": "you did something wrong"})
+
+        with aioresponses() as mocked_server:
+            mocked_server.get(url="https://test.inv/FV2204/UTILMD/000P", callback=simulate_error)
+            actual = await package_resolver.get_condition_expression("000P")
+            assert actual == PackageKeyConditionExpressionMapping(
+                # see the documentation: if the package could not be resolved, you'll get a None package_expression
+                # but the PackageKeyConditionExpressionMapping itself is _not_ None
+                edifact_format=EdifactFormat.UTILMD,
+                package_key="000P",
+                package_expression=None,
+            )
