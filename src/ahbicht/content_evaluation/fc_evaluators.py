6a9207cafcedb77c2fcbacb2bb5790403628c742
konstantin <konstantin.klein@hochfrequenz.de>
2022-06-09 09:09:44 +0200

    🚀 Add a cache for evaluation methods instead on inspection in each call (#176)

--- src/ahbicht/content_evaluation/fc_evaluators.py
+++ src/ahbicht/content_evaluation/fc_evaluators.py
@@ -124,6 +124,7 @@ class DictBasedFcEvaluator(FcEvaluator):
         Initialize with a dictionary that contains all the format constraint evaluation results.
         :param results:
         """
+        super().__init__()
         self._results: Dict[str, EvaluatedFormatConstraint] = results
 
     # pylint: disable=unused-argument
