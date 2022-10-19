fbe906cbd97f413bf3005132077c4de9a206704b
konstantin <konstantin.klein@hochfrequenz.de>
2022-10-19 12:32:24 +0200

    Add `is_valid_expression` Function

---
+++ src/ahbicht/content_evaluation/__init__.py
@@ -0,0 +1,51 @@
+"""
+functions related to content evaluation
+"""
+import asyncio
+from typing import Any, Awaitable, Callable, List
+
+from ahbicht.content_evaluation.content_evaluation_result import ContentEvaluationResult
+from ahbicht.content_evaluation.evaluationdatatypes import EvaluatableData
+from ahbicht.content_evaluation.fc_evaluators import FcEvaluator
+from ahbicht.content_evaluation.rc_evaluators import ContentEvaluationResultBasedRcEvaluator, RcEvaluator
+from ahbicht.content_evaluation.token_logic_provider import TokenLogicProvider
+from ahbicht.expressions.ahb_expression_evaluation import evaluate_ahb_expression_tree
+from ahbicht.expressions.condition_expression_parser import extract_categorized_keys_from_tree
+from ahbicht.expressions.expression_resolver import parse_expression_including_unresolved_subexpressions
+
+
+async def is_valid_expression(
+    ahb_expression: str, content_eval_result_setter: Callable[[ContentEvaluationResult], Any]
+) -> bool:
+    """
+    returns true iff the given expression is both well-formed and valid.
+    Valid means that any evaluation leads to meaningful results.
+    ⚠ This only works if you injected the ContentEvaluationResultBased... FC/RC/Hints/Package logic providers.
+    :param content_eval_result_setter: a method that sets content evaluation result
+    :param ahb_expression: "Muss [1] U [2]" (returns True)  "Muss ([61]u [584]) o[583]" (returns False)
+    :return: True iff the expression is valid
+    """
+    try:
+        tree = await parse_expression_including_unresolved_subexpressions(ahb_expression)
+        categorized_key_extract = extract_categorized_keys_from_tree(tree, sanitize=True)
+    except SyntaxError:
+        return False
+    evaluation_tasks: List[Awaitable] = []
+    for content_evaluation_result in categorized_key_extract.generate_possible_content_evaluation_results():
+
+        async def evaluate_with_cer(cer: ContentEvaluationResult):
+            content_eval_result_setter(cer)
+            try:
+                await evaluate_ahb_expression_tree(tree)
+            except NotImplementedError as not_implemented_error:
+                if "due to missing information" in str(not_implemented_error):
+                    pass
+                else:
+                    raise not_implemented_error  # this is, in general, an indicator for an invalid expression
+
+        evaluation_tasks.append(evaluate_with_cer(content_evaluation_result))
+    try:
+        await asyncio.gather(*evaluation_tasks)
+    except NotImplementedError:
+        return False
+    return True
---
+++ unittests/test_validity_check.py
@@ -0,0 +1,85 @@
+from contextvars import ContextVar
+from typing import Optional
+
+import inject
+import pytest  # type:ignore[import]
+
+from ahbicht.content_evaluation import is_valid_expression
+from ahbicht.content_evaluation.content_evaluation_result import ContentEvaluationResult, ContentEvaluationResultSchema
+from ahbicht.content_evaluation.evaluationdatatypes import EvaluatableData, EvaluatableDataProvider
+from ahbicht.content_evaluation.fc_evaluators import ContentEvaluationResultBasedFcEvaluator
+from ahbicht.content_evaluation.rc_evaluators import ContentEvaluationResultBasedRcEvaluator
+from ahbicht.content_evaluation.token_logic_provider import SingletonTokenLogicProvider, TokenLogicProvider
+from ahbicht.expressions.hints_provider import ContentEvaluationResultBasedHintsProvider
+from ahbicht.expressions.package_expansion import ContentEvaluationResultBasedPackageResolver
+from unittests.defaults import (
+    default_test_format,
+    default_test_version,
+    store_content_evaluation_result_in_evaluatable_data,
+)
+
+_content_evaluation_result: ContextVar[Optional[ContentEvaluationResult]] = ContextVar(
+    "_content_evaluation_result", default=None
+)
+
+
+def _get_evaluatable_data():
+    """
+    returns the _content_evaluation_result context var value wrapped in a EvaluatableData container.
+    This si the kind of data that the ContentEvaluationResultBased RC/FC Evaluators, HintsProvider and Package Resolver
+    require.
+    :return:
+    """
+    cer = _content_evaluation_result.get()
+    return EvaluatableData(
+        edifact_seed=ContentEvaluationResultSchema().dump(cer),
+        edifact_format=default_test_format,
+        edifact_format_version=default_test_version,
+    )
+
+
+class TestValidityCheck:
+    """
+    a test class for the expresison validation feature
+    """
+
+    @pytest.fixture
+    def inject_cer_evaluators(self):
+        fc_evaluator = ContentEvaluationResultBasedFcEvaluator()
+        fc_evaluator.edifact_format = default_test_format
+        fc_evaluator.edifact_format_version = default_test_version
+
+        rc_evaluator = ContentEvaluationResultBasedRcEvaluator()
+        rc_evaluator.edifact_format = default_test_format
+        rc_evaluator.edifact_format_version = default_test_version
+
+        package_resolver = ContentEvaluationResultBasedPackageResolver()
+        package_resolver.edifact_format = default_test_format
+        package_resolver.edifact_format_version = default_test_version
+
+        hints_provider = ContentEvaluationResultBasedHintsProvider()
+        hints_provider.edifact_format = default_test_format
+        hints_provider.edifact_format_version = default_test_version
+
+        def configure(binder):
+            binder.bind(
+                TokenLogicProvider,
+                SingletonTokenLogicProvider([fc_evaluator, rc_evaluator, package_resolver, hints_provider]),
+            )
+            binder.bind_to_provider(EvaluatableDataProvider, _get_evaluatable_data)
+
+        inject.configure_once(configure)
+        yield
+        inject.clear()
+
+    @pytest.mark.parametrize(
+        "ahb_expression,expected_result",
+        [
+            pytest.param("Foo", False),
+            pytest.param("Muss [1] U [2]", True),
+            pytest.param("Muss [61] O [584]", False),  # connecting a hint with LAND is not valid
+        ],
+    )
+    async def test_is_valid_expression(self, ahb_expression: str, expected_result: bool, inject_cer_evaluators):
+        actual = await is_valid_expression(ahb_expression, lambda cer: _content_evaluation_result.set(cer))
+        assert actual == expected_result
