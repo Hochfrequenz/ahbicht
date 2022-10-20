68ee337b85206017ac9880cde44d5fffa724a29c
konstantin <konstantin.klein@hochfrequenz.de>
2022-10-20 09:03:07 +0200

    Update unittests/conftest.py
    Co-authored-by: Annika <73470827+hf-aschloegl@users.noreply.github.com>

--- unittests/conftest.py
+++ unittests/conftest.py
@@ -18,7 +18,7 @@ def store_content_evaluation_result_in_evaluatable_data(
     content_evaluation_result: ContentEvaluationResult,
 ) -> EvaluatableData:
     """
-    a helper method for the tests so store a serialized content evaluation result in an EvaluatableData instance
+    a helper method for the tests to store a serialized content evaluation result in an EvaluatableData instance
     :param content_evaluation_result:
     :return: a new EvaluatableData instance
     """
