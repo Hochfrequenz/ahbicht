599c32c6e717c2ab1d72462075b845b39ca49215
konstantin <konstantin.klein@hochfrequenz.de>
2022-11-03 07:50:33 +0100

    Kevins Challenge

--- src/ahbicht/__init__.py
+++ src/ahbicht/__init__.py
@@ -1,6 +1,7 @@
 """
 AHBicht is a lark based parser and evaluation framework for conditions that occur in Anwendungshandbüchern (AHB).
 """
+import sys
 from enum import Enum
 
 
@@ -10,17 +11,17 @@ class StrEnum(str, Enum):
     This is a workaround for Python <v3.11 because enum.StrEnum was introduced in Python 3.11.
     """
 
+    # We'll live with this  class for Python <v3.11. The unit test for python 3.9-3.11 ensure that this works.
 
-try:
+
+if sys.version_info.major == 3 and sys.version_info.minor >= 11:
+    # but for python >=v3.11 we'll use the builtin StrEnum from the std library
     from enum import StrEnum as std_lib_strenum
 
     StrEnum = std_lib_strenum  # type:ignore[misc, assignment]
+
     # We have to use the builtin / std lib enum.StrEnum in Python >= 3.11, because the behaviour of (str,Enum) changed:
     # class Foo(str, Enum):
     #     MEMBER = "MEMBER"
     # f"{a_str_enum_member}" results in "MEMBER" for Python < v3.11 but "Foo.MEMBER" in Python >= v3.11
     # Using the old (str, Enum) in Python 3.11 causes errors e.g. in the ExpressionBuilders.
-except ImportError:
-    # This import error occurs for python < 3.11
-    # We'll live with the fallback class defined above. The unit test for python 3.9-3.11 ensure that this works.
-    pass
