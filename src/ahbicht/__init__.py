b5027ef0ef96da6cb7a31d47bead2bac742f636e
konstantin <konstantin.klein@hochfrequenz.de>
2022-11-03 08:02:50 +0100

    don't re-name import
    less explicit imo

--- src/ahbicht/__init__.py
+++ src/ahbicht/__init__.py
@@ -5,9 +5,7 @@ import sys
 from enum import Enum
 
 if sys.version_info.major == 3 and sys.version_info.minor >= 11:
-    from enum import StrEnum as std_lib_strenum
-
-    StrEnum = std_lib_strenum  # type:ignore[misc, assignment]
+    from enum import StrEnum
 
     # We have to use the builtin / std lib enum.StrEnum in Python >= 3.11, because the behaviour of (str,Enum) changed:
     # class Foo(str, Enum):
