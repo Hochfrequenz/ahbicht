63e3581ce918c0158402b5ed95e85cdd4c07a0a7
DeltaDaniel <139119540+DeltaDaniel@users.noreply.github.com>
2024-12-16 16:56:09 +0100

    added test

--- unittests/test_time_condition_resolver.py
+++ unittests/test_time_condition_resolver.py
@@ -14,10 +14,11 @@ class TestTimeConditionReplacement:
     """
 
     @pytest.mark.parametrize(
-        "expression, replace_time_conditions, expected_tree",
+        "expression, replace_time_conditions, resolve_time_conditions, expected_tree",
         [
             pytest.param(
                 "[UB1] U [42]",
+                False,
                 False,
                 Tree(  # type:ignore[misc]
                     "and_composition",
@@ -30,6 +31,7 @@ class TestTimeConditionReplacement:
             pytest.param(
                 "[UB1] U [42]",
                 True,
+                True,
                 Tree(  # type:ignore[misc]
                     "and_composition",
                     [
@@ -41,6 +43,7 @@ class TestTimeConditionReplacement:
             pytest.param(
                 "[UB2] U [42]",
                 False,
+                False,
                 Tree(  # type:ignore[misc]
                     "and_composition",
                     [
@@ -51,17 +54,61 @@ class TestTimeConditionReplacement:
             ),
             pytest.param(
                 "[UB2] U [42]",
-                True,
+                False,
+                False,
                 Tree(  # type:ignore[misc]
                     "and_composition",
                     [
-                        Tree(Token("RULE", "condition"), [Token("CONDITION_KEY", "934")]),
+                        Tree(Token("RULE", "time_condition"), [Token("TIME_CONDITION_KEY", "UB2")]),
+                        Tree(Token("RULE", "condition"), [Token("CONDITION_KEY", "42")]),
+                    ],
+                ),
+            ),
+            pytest.param(
+                "[UB2] U [42]",
+                False,
+                True,
+                Tree(
+                    "and_composition",
+                    [
+                        Tree(
+                            "xor_composition",
+                            [
+                                Tree(
+                                    "and_composition",
+                                    [
+                                        Tree(Token("RULE", "condition"), [Token("CONDITION_KEY", "931")]),
+                                        Tree(
+                                            "then_also_composition",
+                                            [
+                                                Tree(Token("RULE", "condition"), [Token("CONDITION_KEY", "934")]),
+                                                Tree(Token("RULE", "condition"), [Token("CONDITION_KEY", "490")]),
+                                            ],
+                                        ),
+                                    ],
+                                ),
+                                Tree(
+                                    "and_composition",
+                                    [
+                                        Tree(Token("RULE", "condition"), [Token("CONDITION_KEY", "931")]),
+                                        Tree(
+                                            "then_also_composition",
+                                            [
+                                                Tree(Token("RULE", "condition"), [Token("CONDITION_KEY", "935")]),
+                                                Tree(Token("RULE", "condition"), [Token("CONDITION_KEY", "491")]),
+                                            ],
+                                        ),
+                                    ],
+                                ),
+                            ],
+                        ),
                         Tree(Token("RULE", "condition"), [Token("CONDITION_KEY", "42")]),
                     ],
                 ),
             ),
             pytest.param(
                 "[UB3] U [42]",
+                False,
                 False,
                 Tree(  # type:ignore[misc]
                     "and_composition",
@@ -73,6 +120,7 @@ class TestTimeConditionReplacement:
             ),
             pytest.param(
                 "[UB3] U [42]",
+                True,
                 True,
                 Tree(  # type:ignore[misc]
                     "and_composition",
@@ -103,9 +151,9 @@ class TestTimeConditionReplacement:
         ],
     )
     async def test_time_condition_expansion(
-        self, expression: str, replace_time_conditions: bool, expected_tree: Tree[Token]
+        self, expression: str, replace_time_conditions: bool, resolve_time_conditions: bool, expected_tree: Tree[Token]
     ):
         parsed_tree = await parse_expression_including_unresolved_subexpressions(
-            expression, replace_time_conditions=replace_time_conditions, resolve_time_conditions=replace_time_conditions
+            expression, replace_time_conditions=replace_time_conditions, resolve_time_conditions=resolve_time_conditions
         )
         assert parsed_tree == expected_tree
