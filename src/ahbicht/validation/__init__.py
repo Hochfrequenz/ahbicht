835a032987a3185fdb5195a9457235c4d85d67b0
Konstantin <konstantin.klein+github@hochfrequenz.de>
2025-05-14 13:32:28 +0200

    Drop validation (now happens in wanna.bee); Remove maus-dependency

--- src/ahbicht/validation/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-instantiates a logger for all validation purposes
-"""
-
-import logging
-
-validation_logger = logging.getLogger("ahbicht.validation")
-validation_logger.setLevel(logging.DEBUG)
